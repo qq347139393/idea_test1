5389be859515f8821ea5d8517098c9f8a684e47e
qq347139393 <347139393@qq.com>
2019-02-17 11:31:19 +0800

    the first commit project by Git.

---
+++ src/cn/demo/Test.java
@@ -0,0 +1,13 @@
+package cn.demo;
+
+/**
+ * @Auther: customer
+ * @Date: 2019/2/17
+ * @Description: cn.demo
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("hello");
+    }
+}
